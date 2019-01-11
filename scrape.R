83dad386ad01b6f44acb57f54fbce888b408a927
Susan Vanderplas <srvanderplas@gmail.com>
2019-01-11 15:08:09 -0600

    initial code, data

---
+++ scrape.R
@@ -0,0 +1,17 @@
+#!/usr/bin/Rscript
+
+library(rvest)
+library(tidyverse)
+
+new_data <- read_html("http://www.police.iastate.edu/content/daily-crime-log") %>%
+  html_node("table") %>%
+  html_table() %>%
+  mutate(scraped = lubridate::today())
+
+if (file.exists("isu_crime_data.csv")) {
+  data <- read_csv("isu_crime_data.csv") %>%
+    bind_rows(new_data)
+  write_csv(data, "isu_crime_data.csv", append = F)
+} else {
+  write_csv(new_data, "isu_crime_data.csv", append = F)
+}
