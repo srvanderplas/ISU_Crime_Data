9bcd6e5ee43c91c35250dc68e48e47ad52944305
Susan Vanderplas <srvanderplas@gmail.com>
2019-08-08 14:50:07 -0500

    Automatic Update

--- scrape.R
+++ scrape.R
@@ -3,7 +3,8 @@
 library(here)
 library(rvest)
 library(tidyverse)
-library(tabulizer)
+library(lubridate)
+
 
 # Get wd of script
 if (Sys.info()[['nodename']] == 'bigfoot') {
@@ -16,14 +17,22 @@ new_log <- read_html("https://www.police.iastate.edu/crime-log/") %>%
   html_nodes("a.log") %>%
   html_attr("href")
 
+# get past files
+# dates <- lubridate::today() - lubridate::days(1:30)
+# new_logs <- sprintf("http://www.police.iastate.edu/wp-content/uploads/%d/%d/%d-Crime-Log-%d_%d_%d.pdf",
+#                     year(dates), month(dates), year(dates), month(dates), day(dates), year(dates) %% 2000)
+# download.file(new_logs, destfile = paste0(dates, ".pdf"), mode = 'wb')
 download.file(new_log, destfile = paste0(lubridate::today(), ".pdf"), mode = 'wb')
-tabs <- extract_tables(new_log)
+
+library(tabulizer)
+
+tabs <- tabulizer::extract_tables(new_log)
 
 nm <- c("Case Number", "Classification", "Date Reported", "Time Reported", "Earliest Occurrence Date", "Earliest Occurrence Time", "Latest Occurrence Date", "Latest Occurrence Time", "General Location", "Disposition")
-new_data <- purrr::map(tabs, as_tibble) %>%
-  purrr::map(set_names, nm) %>%
-  bind_rows() %>%
-  filter(row_number() != 1) %>%
+new_data <- purrr::map_df(tabs, as_tibble) %>%
+  select(which(colSums(!is.na(.)) > 0))
+  set_names(nm) %>%
+  filter(`Case Number` != "Case Number") %>%
   mutate_all(str_trim) %>%
   mutate_at(vars(matches("Time")), str_replace_na, replacement = "0000") %>%
   mutate_at(vars(matches("Time")), ~sprintf("%04d", as.numeric(.))) %>%
